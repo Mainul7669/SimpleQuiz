1aeeca8f828277951d08f8c6148f49c27a0f514e
Mainul7669 <mainul7669@gmail.com>
2023-03-16 01:19:28 +0600

    local storage set

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -111,7 +111,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   let storage = JSON.parse(localStorage.getItem("result"));
   if (storage) {
     localStorage.setItem(
-      "result",
+      "results",
       JSON.stringify([
         ...storage,
         {
